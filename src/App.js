4a1e777bd94c113c62e9fd4136d9ab0ccf81cf77
GareevaMilena <61157126+GareevaMilena@users.noreply.github.com>
2020-02-29 19:22:40 +0300

    adding mask overlay
    add mask overlay on face

--- src/App.js
+++ src/App.js
@@ -1,53 +1,8 @@
-/*import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
-// **********************************************
-// Imports
-// **********************************************
-
-import React from 'react';
-//import ReactDOM from 'react-dom';
-
-import {getLandmarks, loadModels, maskify} from './faceapi';
-//import FaceDraw from './facedraw';
-//import ImageSelector from './tool';
-
+import React, {useEffect} from 'react';
+import {getLandmarks, loadModels} from './faceapi';
 import face001 from './face0001.jpg';
-//import face002 from './face002.jpg';
-//import face003 from './face003.jpg';
-//import face004 from './face004.jpg';
-
-// **********************************************
-// Const
-// **********************************************
-
-const WAIT_Y_MIN = 60;
-const WAIT_Y_MAX = 80;
-const WAIT_STEP = 2;
-
+import logo1 from './overlayBLUSH1.png';
+import "./styles.css"
 // **********************************************
 // Vars
 // **********************************************
@@ -62,62 +17,16 @@ class App extends React.Component {
     super(props);
     this.m_mount = null;
     this.onButton = this.onButton.bind(this);
-
+    console.log('CHECK constuctor');
 
   } // end constr
 
-  // *********************************************
-  onTimer() {
-    // console.log('on timer...');
-    if (this.state.fullDesc != null) {
-      clearInterval(this.state.idTimer);
-      this.setState({idTimer: null});
-
-
-
-      //const faceDraw = new FaceDraw();
-      //faceDraw.drawLandmarks(this.state);
-
-      return;
-    }
-
-    const objCanvas = this.state.canvas;
-    const ctx = objCanvas.getContext('2d');
-
-    //const imageSrc = this.refs.face001;
-    const imageSrc = this.state.imageSrc;
-
-    const wImage = imageSrc.width;
-    const hImage = imageSrc.height;
-
-    ctx.drawImage(imageSrc, 0, 0, wImage, hImage, 0, 0, wImage, hImage);
-
-    ctx.font = "48px Arial";
-    ctx.fillStyle = "white";
-    ctx.textAlign = "center";
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    let y = this.state.waitY;
-    ctx.fillText("Wait face detection ...", objCanvas.width / 2, y);
-    y += this.state.waitStep;
-    if (y >= WAIT_Y_MAX)
-    {
-      y = WAIT_Y_MAX;
-      this.setState({waitStep: -WAIT_STEP});
-    }
-    if (y <= WAIT_Y_MIN)
-    {
-      y = WAIT_Y_MIN;
-      this.setState({waitStep: +WAIT_STEP});
-    }
-    this.setState({waitY: y});
-  }
 
   // *********************************************
   renderSceneLoaded(imageSrc) {
     const wImage = imageSrc.width;
     const hImage = imageSrc.height;
-    console.log("image dim = " + wImage.toString() + ", " + hImage.toString());
+    //console.log("image dim = " + wImage.toString() + ", " + hImage.toString());
 
     this.setState({imageSrc: imageSrc});
 
@@ -126,29 +35,18 @@ class App extends React.Component {
       return;
     }
     this.setState({canvas: objCanvas});
-    objCanvas.width = wImage;
-    objCanvas.height = hImage;
 
-    const ctx = objCanvas.getContext('2d');
-
-    ctx.fillStyle = 'rgb(64, 64, 64)';
-    //ctx.fillRect(0,0, wImage, hImage);
-
-    // draw source image on screen
-    ctx.drawImage(imageSrc, 0, 0, wImage, hImage, 0, 0, wImage, hImage);
-
+    console.log('CHECK render scene loaded');
     // perform animtaion by timer
 
-    const idTimerNew = setInterval(this.onTimer.bind(this), 50);
-    this.setState({idTimer: idTimerNew});
   }
 
   renderScene(faceRef) {
     const imageSrc = faceRef;
-
+    console.log('CHECK render scene');
     imageSrc.onload = () => {
       this.renderSceneLoaded(imageSrc);
-    } // end on load image
+    }// end on load image
   } // end renderScene
 
   // *********************************************
@@ -156,58 +54,61 @@ class App extends React.Component {
     this.renderScene(this.refs.face001);
 
     await loadModels();
+    console.log('CHECK did mount');
     await this.processImage(this.state.imageSrc);
   }
 
   processImage = async(imageSrc) => {
-    await getLandmarks(imageSrc).then( fullDescription => {
+    await getLandmarks(imageSrc, this.state).then( fullDescription => {
       // console.log(fullDescription);
+      console.log('imag', imageSrc);
+      console.log('CHECK process Image');
 
-      maskify(this.refs.face001);
       this.setState({fullDesc: fullDescription});
     });
   };
-
   async startDetect(index) {
-    const refs = [this.refs.face001];
+    const refs = [this.refs.face001, this.refs.face002];
     const imgRef = refs[index];
     this.setState({fullDesc: null});
     this.renderSceneLoaded(imgRef);
-    // await this.processImage(this.state.imageSrc);
+    console.log('CHECK start Detect');
+
     await this.processImage(imgRef);
   }
 
-  onButton(index) {
-    console.log('Pressed button :' + index.toString());
-    //const imgArr = [face001, face002, face003, face004];
-    //const imgSelected = imgArr[index];
-    //this.setState({imagSrc: imgSelected});
-    this.startDetect(index);
+  onButton() {
+    //console.log('Pressed button :' + index.toString());
+    console.log('CHECK onButton');
+    //this.startDetect(index);
   }
+
+//upper
+
+
+onClickChoose(){
+
+}
 
   // *********************************************
   render() {
+
     const styleImage = {
       display: 'none'
     };
 
-    const jsxCanvas = <canvas ref="canvas" />
-    const jsxRender =     <div className="App">
+    console.log('CHECK render');
+    //const jsxCanvas = <canvas ref="canvas" />
+    return <div className="App">
       <header className="App-header">
 
-        <img src={face001} alt="face" style={styleImage} ref="face001" />
-        <img
-            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=750&q=60"
-            alt="A woman wearing a red sweater."
-        />
+        <canvas id="canvas" width="0" height="0"></canvas>
 
 
-
-        {jsxCanvas}
+        <img src={face001} alt="face" ref="face001"/>
+        <img src={logo1} alt="fe" ref="logo" id="or" hidden={true}/>
       </header>
-    </div>
-
-    return jsxRender;
+    </div>;
   } // end render
 
 
